daa02f4f28c088b8e673fd0560dd245da0b37486
PavloZaluskyy <pavlozaluskiy@mail.com>
2020-06-06 00:45:31 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -23,7 +23,7 @@ import { MainBottomComponent } from './component/main-chat/main-bottom/main-bott
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
